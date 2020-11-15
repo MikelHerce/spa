0a4215c77850d88d2290cbc55b93232d2b1ab460
Mikel <mikelherce87@gmail.com>
2020-11-15 02:08:07 +0100

    navegacion y router active

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
@@ -8,6 +9,7 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'heroes', component: HeroesComponent },
     { path: 'about', component: AboutComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
